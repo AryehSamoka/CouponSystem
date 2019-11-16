8a65deede3d78747dbdf8af8221b6059844878a3
Aryeh Samoka <aryeh.samoka@gmail.com>
2019-11-16 20:08:27 +0200

    tokens cleaner adjusted with sleep.

--- src/main/java/com/aryeh/CouponSystem/threads/ClientSessionCleanerTask.java
+++ src/main/java/com/aryeh/CouponSystem/threads/ClientSessionCleanerTask.java
@@ -1,31 +1,45 @@
 package com.aryeh.CouponSystem.threads;
 
 import com.aryeh.CouponSystem.rest.ClientSession;
+
 import java.util.Iterator;
 import java.util.Map;
 
-    public class ClientSessionCleanerTask implements Runnable {
-        private boolean run = true;
-        private static final long MINUTE_IN_MILLIS = 60 * 1_000;
-        private Map<String, ClientSession> tokensMap;
+public class ClientSessionCleanerTask implements Runnable {
+    private boolean run = true;
+    private static final long MINUTE_IN_MILLIS = 60 * 1_000;
+    private static final long SECOND_IN_MILLIS = 1_000;
+    private static final long SECOND_IN_MINUTES = 60;
+    private Map<String, ClientSession> tokensMap;
 
-        public ClientSessionCleanerTask(Map<String, ClientSession> tokensMap) {
-            this.tokensMap = tokensMap;
-        }
+    public ClientSessionCleanerTask(Map<String, ClientSession> tokensMap) {
+        this.tokensMap = tokensMap;
+    }
 
-        @Override
-        public void run() {
+    @Override
+    public void run() {
+        String token;
+        ClientSession session;
+        long TimeMillisBeforeForLoop;
+        Iterator<String> it;
 
-            while (run) {
-                Iterator<String> it1 = tokensMap.keySet().iterator();
-                while(it1.hasNext()){
-                    String key = it1.next();
-                    ClientSession session = tokensMap.get(key);
-                    boolean minuteAccessed = System.currentTimeMillis() - session.getLastAccessedMillis() > MINUTE_IN_MILLIS;
-                    if (minuteAccessed) {
-                        tokensMap.remove(key);
+        while (run) {
+            TimeMillisBeforeForLoop = System.currentTimeMillis();
+            for (int i = 1; i <= SECOND_IN_MINUTES; i++) {
+                try {
+                    Thread.sleep( i*SECOND_IN_MILLIS + TimeMillisBeforeForLoop - System.currentTimeMillis());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                it = tokensMap.keySet().iterator();
+                while (it.hasNext()) {
+                    token = it.next();
+                    session = tokensMap.get(token);
+                    if (System.currentTimeMillis() - session.getLastAccessedMillis() > MINUTE_IN_MILLIS) {
+                        tokensMap.remove(token);
                     }
                 }
             }
         }
     }
+}
