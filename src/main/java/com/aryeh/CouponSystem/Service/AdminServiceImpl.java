eac5b281e885d03bac98bfb970f8788ec473632c
Aryeh Samoka <aryeh.samoka@gmail.com>
2019-12-01 14:45:29 +0200

    old

--- src/main/java/com/aryeh/CouponSystem/Service/AdminServiceImpl.java
+++ src/main/java/com/aryeh/CouponSystem/Service/AdminServiceImpl.java
@@ -1,13 +1,19 @@
 package com.aryeh.CouponSystem.Service;
 
-import com.aryeh.CouponSystem.data.entity.*;
-import com.aryeh.CouponSystem.data.repository.*;
+import com.aryeh.CouponSystem.data.entity.Admin;
+import com.aryeh.CouponSystem.data.entity.Company;
+import com.aryeh.CouponSystem.data.entity.Coupon;
+import com.aryeh.CouponSystem.data.entity.Customer;
+import com.aryeh.CouponSystem.data.repository.AdminRepository;
+import com.aryeh.CouponSystem.data.repository.CompanyRepository;
+import com.aryeh.CouponSystem.data.repository.CouponRepository;
+import com.aryeh.CouponSystem.data.repository.CustomerRepository;
 import com.aryeh.CouponSystem.rest.ex.InvalidRootAdminAccessException;
 import com.aryeh.CouponSystem.rest.ex.NoSuchCompanyException;
 import com.aryeh.CouponSystem.rest.ex.NoSuchCustomerException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -26,32 +32,24 @@ public class AdminServiceImpl extends AbsService implements AdminService {
     private final CustomerRepository customerRepository;
     private AdminRepository adminRepository;
     private Environment env;
-    private UserRepository userRepository;
-    private CompanyServiceImpl companyServiceImpl;
-    private CustomerServiceImpl customerServiceImpl;
-    private static final int ADMIN_ROLE = -1;
+    private ApplicationContext context;
 
 
     @Autowired
     public AdminServiceImpl(CompanyRepository companyRepository, CouponRepository couponRepository,
                             CustomerRepository customerRepository, AdminRepository adminRepository,
-                            Environment env, UserRepository userRepository, CompanyServiceImpl companyServiceImpl,
-                            CustomerServiceImpl customerServiceImpl) {
+                            Environment env, ApplicationContext context) {
         this.companyRepository = companyRepository;
         this.couponRepository = couponRepository;
         this.customerRepository = customerRepository;
         this.adminRepository = adminRepository;
         this.env = env;
-        this.userRepository = userRepository;
-        this.companyServiceImpl = companyServiceImpl;
-        this.customerServiceImpl = customerServiceImpl;
+        this.context = context;
     }
 
     @PostConstruct
     public void init() {
         Admin rootAdminDB = insertRootAdmin();
-
-        insertRootUser(rootAdminDB);
 
         rootId = rootAdminDB.getId();
     }
@@ -73,9 +71,6 @@ public class AdminServiceImpl extends AbsService implements AdminService {
 
                 admin.setId(0);
                 Admin adminNew = adminRepository.save(admin);
-
-                userRepository.save(new User(adminNew));
-
                 return adminNew;
             } else {
                 throw new InvalidRootAdminAccessException("");
@@ -94,15 +89,7 @@ public class AdminServiceImpl extends AbsService implements AdminService {
     @Override
     @Transactional
     public void deleteById() {
-        Admin admin = findById();
-
         if (adminId != rootId) {
-            Optional<User> optUser = userRepository.findByEmailAndPassword(admin.getEmail(), admin.returnPassword());
-
-            if (optUser.isPresent()) {
-                userRepository.deleteById(optUser.get().getId());
-            }
-
             adminRepository.deleteById(adminId);
         } else {
             throw new InvalidRootAdminAccessException("");
@@ -133,7 +120,6 @@ public class AdminServiceImpl extends AbsService implements AdminService {
             }
 
             Company companyNew = companyRepository.save(company);
-            userRepository.save(new User(companyNew));
             return companyNew;
         }
         return Company.empty();
@@ -141,7 +127,8 @@ public class AdminServiceImpl extends AbsService implements AdminService {
 
     @Override
     @Transactional
-    public Company updateCompany(Company company){
+    public Company updateCompany(Company company) {
+        CompanyServiceImpl companyServiceImpl = context.getBean(CompanyServiceImpl.class);
         companyServiceImpl.setCompanyId(company.getId());
         return companyServiceImpl.update(company);
     }
@@ -149,7 +136,8 @@ public class AdminServiceImpl extends AbsService implements AdminService {
     @Override
     @Transactional
     public Company deleteCompanyById(long companyId) {
-        deleteCompanyUser(checkCompany(companyId));
+        checkCompany(companyId);
+        companyRepository.deleteById(companyId);
         return Company.empty();
     }
 
@@ -169,14 +157,12 @@ public class AdminServiceImpl extends AbsService implements AdminService {
     @Transactional
     public Customer createCustomer(Customer customer) {
         if (customer != null) {
-
             customer.setId(0);
+
             /*A customer can't be created with coupons.*/
             customer.setCoupons(Collections.emptyList());
 
             Customer customerNew = customerRepository.save(customer);
-
-            userRepository.save(new User(customerNew));
             return customerNew;
         }
         return Customer.empty();
@@ -184,7 +170,8 @@ public class AdminServiceImpl extends AbsService implements AdminService {
 
     @Override
     @Transactional
-    public Customer updateCustomer(Customer customer){
+    public Customer updateCustomer(Customer customer) {
+        CustomerServiceImpl customerServiceImpl = context.getBean(CustomerServiceImpl.class);
         customerServiceImpl.setCustomerId(customer.getId());
         return customerServiceImpl.update(customer);
     }
@@ -192,7 +179,8 @@ public class AdminServiceImpl extends AbsService implements AdminService {
     @Override
     @Transactional
     public Customer deleteCustomerById(long customerId) {
-        deleteCustomerUser(checkCustomer(customerId));
+        checkCustomer(customerId);
+        customerRepository.deleteById(customerId);
         return Customer.empty();
     }
 
@@ -227,96 +215,33 @@ public class AdminServiceImpl extends AbsService implements AdminService {
         Admin rootAdmin = new Admin(env.getProperty("adminRoot.username"), env.getProperty("adminRoot.password"));
         Optional<Admin> optionalAdmin = adminRepository.findByEmail(rootAdmin.getEmail());
 
-        Admin rootAdminDB;
-        if (!optionalAdmin.isPresent()) {
-            rootAdminDB = adminRepository.save(rootAdmin);
-        } else {
+        if (optionalAdmin.isPresent()) {
             rootAdmin.setId(optionalAdmin.get().getId());
-            rootAdminDB = adminRepository.save(rootAdmin);
         }
-        return rootAdminDB;
-    }
-
-    private void insertRootUser(Admin rootAdminDB) {
-        User rootUser = new User(rootAdminDB);
-
-        Optional<User> optionalRootUser = userRepository.findByEmailAndRole(rootAdminDB.getEmail(), ADMIN_ROLE);
-
-        if (!optionalRootUser.isPresent())
-            userRepository.save(rootUser);
-        else {
-            rootUser.setId(optionalRootUser.get().getId());
-            userRepository.save(rootUser);
-        }
+        return adminRepository.save(rootAdmin);
     }
 
     private Admin updateAdmin(Admin admin) {
         if (adminId != rootId) {
-
-            Admin oldAdmin = findById();
-            checkPassword(admin, oldAdmin);
-            updateAdminUser(admin, oldAdmin);
-
+            admin.checkPassword(findById());
             return adminRepository.save(admin);
         } else {
             throw new InvalidRootAdminAccessException("");
         }
     }
 
-    private void updateAdminUser(Admin admin, Admin oldAdmin) {
-        Optional<User> optUser = userRepository.findByEmailAndPassword(oldAdmin.getEmail(), oldAdmin.returnPassword());
-        /*The user has to be present because we got here with token*/
-        User user = optUser.get();
-        user.setEmail(admin.getEmail());
-        user.setPassword(admin.getPassword());
-        userRepository.save(user);
-    }
-
-    /**
-     * The password will be checked and if it is null the old password will stay,
-     * the reason is that only the password can't be seen by findById because of json ignore.
-     * On client to check what will be counted as null.
-     * @param admin
-     * @param oldAdmin
-     */
-    private void checkPassword(Admin admin, Admin oldAdmin) {
-        if(admin.getPassword() == null){
-            admin.setPassword(oldAdmin.getPassword());
-        }
-    }
-
-    private Optional<Customer> checkCustomer(long customerId) {
+    private void checkCustomer(long customerId) {
         Optional<Customer> optionalCustomer = customerRepository.findById(customerId);
         if (!optionalCustomer.isPresent()) {
             throw new NoSuchCustomerException("");
         }
-        return optionalCustomer;
     }
 
-    private void deleteCustomerUser(Optional<Customer> optionalCustomer) {
-        Customer customer = optionalCustomer.get();
-        Optional<User> optUser = userRepository.findByEmailAndPassword(customer.getEmail(), customer.returnPassword());
-
-        if (optUser.isPresent()) {
-            userRepository.deleteById(optUser.get().getId());
-        }
-    }
-
-    private Optional<Company> checkCompany(long companyId) {
+    private void checkCompany(long companyId) {
         Optional<Company> optionalCompany = companyRepository.findById(companyId);
         if (!optionalCompany.isPresent()) {
 
             throw new NoSuchCompanyException("");
-        }
-        return optionalCompany;
-    }
-
-    private void deleteCompanyUser(Optional<Company> optionalCompany) {
-        Company company = optionalCompany.get();
-        Optional<User> optUser = userRepository.findByEmailAndPassword(company.getEmail(), company.returnPassword());
-
-        if (optUser.isPresent()) {
-            userRepository.deleteById(optUser.get().getId());
         }
     }
 }
