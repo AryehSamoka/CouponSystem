d77432ee7ef6bac9b1fdbb3dfbe6bf74c1b9495d
Aryeh Samoka <aryeh.samoka@gmail.com>
2019-12-25 11:52:26 +0200

    organized imports.

--- src/main/java/com/aryeh/CouponSystem/rest/controller/LoginController.java
+++ src/main/java/com/aryeh/CouponSystem/rest/controller/LoginController.java
@@ -4,7 +4,6 @@ import com.aryeh.CouponSystem.rest.ClientSession;
 import com.aryeh.CouponSystem.rest.CsSystem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
